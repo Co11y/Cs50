f30b06d7cefc962757e3bcf8cbbd786ca6ab9ec3
Colly <kostia4567@gmail.com>
2018-09-09 07:28:01 +0300

    Rename number --> height

--- prac2(mario).c
+++ prac2(mario).c
@@ -2,19 +2,19 @@
 #include <string.h>
 #include <cs50.h>
 int main(void) {
-	int number;
+	int height;
 	int lenght;
 	// number input and check
 	do
 	{
-		number = get_int("Height:");
-	} while ((number > 23) || (number < 0));
+		height = get_int("Height:");
+	} while ((height > 23) || (height < 0));
 	// first loop, i  - main counter, 2 couze top should be flat
 	for (int i = 2; i < number + 2; i++) {
 		// l = i - 1, because of usseless " " in the end
 		int l = i - 1;
 		// print spaces
-		for (lenght = number; lenght > l; lenght--) {
+		for (lenght = height; lenght > l; lenght--) {
 			printf(" ");
 		}
 		// print #
