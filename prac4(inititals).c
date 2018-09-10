049ed60169a9605a8c33022473fbc81108e1adb9
Colly <kostia4567@gmail.com>
2018-09-10 20:23:42 +0300

    better than nothing

--- prac4(inititals).c
+++ prac4(inititals).c
@@ -2,31 +2,30 @@
 #include <cs50.h>
 #include <string.h>
 int main(void) {
+	char initials[3];
+	char initials_up[3];
+	int int_initial = 0;
+	char char_initial;
 	// getting string
 	string name = get_string("print your name here: "); \
 		// def string lenght for loop
 		int lenght = strlen(name);
-	char initials[3];
 	int j = 1;
-	int int_initial = 0;
-	char char_initial;
 	// getting first initial
 	initials[0] = name[0];
 	for (int i = 0; i < lenght; i++) {
 		char charc = name[i];
-		printf("before_If %c\n", charc);
 		if (charc == ' ') {
 			initials[j] = name[i + 1];
-			//  printf("in if %c number in array %d\n", initials[j], j);
 			j++;
 		}
 	}
-	for (int n = 0; n < 3; n++) {
-		// printf("initial %c\n", initials[n]);
-		int_initial = (int)initials[n];
-		int_initial = int_initial + 26;
+	for (int cnt = 0; cnt < 3; cnt++) {
+		int_initial = (int)initials[cnt];
+		int_initial = int_initial - 32;
 		char_initial = (char)int_initial;
-		printf("%c", char_initial);
+		initials_up[cnt] = char_initial;
 	}
 	printf("%c%c%c\n", initials[0], initials[1], initials[2]);
+	printf("%c%c%c\n", initials_up[0], initials_up[1], initials_up[2]);
 }
